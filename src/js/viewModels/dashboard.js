e21fbf5526cb2cf88dfad8782342faaf71498166
Livier2492 <livier.hernandez@oracle.com>
2017-11-01 18:28:49 -0600

    Fix comment line

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,10 +1,8 @@
-**
+/*
  * Copyright (c) 2014, 2017, Oracle and/or its affiliates.
  * The Universal Permissive License (UPL), Version 1.0
  */
-/*
- * Your dashboard ViewModel code goes here
- */
+
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojselectcombobox'
     , 'ojs/ojbutton', 'promise', 'ojs/ojtable', 'ojs/ojarraytabledatasource',
 'ojs/ojinputtext', 'ojs/ojinputnumber', 'ojs/ojlabel'],
